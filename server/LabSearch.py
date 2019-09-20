c0bad53b37c21ce8d684b02589afc33e59d32cad
egorix2000 <egorix2000@mail.ru>
2019-09-20 11:57:14 +0300

    Add server made by Alexandr Fricler

---
+++ server/LabSearch.py
@@ -0,0 +1,72 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+import pandas as pd
+import numpy as np
+from flask import request, jsonify
+
+#app.secret_key = b'\x99\xa4\x11\xdc\xe5\xef\xac\xf3\x1f"(\x8bD]"\xf7'
+
+lab_data = pd.read_csv(str('labdata.csv'), index_col=0, header=None).T
+
+lab_data['city'] = 'Moscow' # It's not in the database now
+
+@app.route('/cities', methods=['GET', 'POST'])
+def getCities():
+    cities = list(np.unique(lab_data['city'].values))
+    answer = {'cities': cities}
+    return jsonify(answer)
+
+
+@app.route('/universities', methods=['GET', 'POST'])
+def getUniversities():
+    input_json = request.get_json(force=True)
+    city = input_json['city']
+    labs_city = lab_data[lab_data['city'] == city]
+    unis = list(np.unique(labs_city['organisation'].dropna().values))
+    answer = {'universities': unis}
+    return jsonify(answer)
+
+
+@app.route('/subjects', methods=['GET', 'POST'])
+def getSubjects():
+    input_json = request.get_json(force=True)
+    city = input_json['city']
+    university = input_json['organisation']
+    labs_city = lab_data[lab_data['city'] == city]
+    labs_city_uni = labs_city[labs_city['organisation'] == university]
+    subjects = list(np.unique(labs_city_uni['field'].dropna().values))
+    answer = {'subjects': subjects}
+    return jsonify(answer)
+
+@app.route('/top', methods=['GET', 'POST'])
+def getTop():
+    input_json = request.get_json(force=True)
+    city = input_json['city']
+    university = input_json['organisation']
+    subject = input_json['subject']
+
+    labs_city = lab_data[lab_data['city'] == city]
+    labs_city_uni = labs_city[labs_city['organisation'] == university]
+    labs_city_uni_subj = labs_city_uni[labs_city_uni['field'] == subject]
+
+    searchText = input_json.get('searchText')
+
+    labs_answer = [{'name': labs_city_uni_subj['title'].values[i],
+                   'university': labs_city_uni_subj['organisation'].values[i],
+                   'head': labs_city_uni_subj['head of the lab'].values[i],
+                    'keywords': labs_city_uni_subj['keywords'].values[i]}
+                    for i in range(labs_city_uni_subj.shape[0])]
+
+    answer = {'laboratories': labs_answer}
+    return jsonify(answer)
+
+if __name__ == '__main__':
+    app.run()
